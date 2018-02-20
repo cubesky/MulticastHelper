c2b3590d64fd9a5cc9e7fcda9548a630b261fa0a
CubeSky <max_301@live.com>
2018-02-20 18:48:14 +0800

    Version 1.0

---
+++ src/test/java/MulticastTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class MulticastTest {
+    @Test
+    public void MulticastTest_1() throws IOException {
+        MulticastHelper helper = new MulticastHelper("224.224.224.2", 9999);
+        helper.setCallback((address,port,array) -> {
+            System.out.println(new String(array));
+            try {
+                helper.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        helper.receiveMulticast();
+        MulticastHelper.sendMulticast("224.224.224.2",9999,"Test".getBytes());
+    }
+}
